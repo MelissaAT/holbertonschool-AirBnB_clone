399e52142da9f36fd69d901adc7a4ca76611f2bf
yahdielo <yahdielaz@gmail.com>
2023-02-18 16:55:13 -0400

    sotre object in storage and reload

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,8 @@
 import uuid
 from datetime import datetime
 
+import models
+
 
 class BaseModel:
     """Class that defines all common
@@ -16,12 +18,14 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
+        models.storage.reload()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
---
+++ models/engine/file_storage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Documentation Module"""
+
+import json
+
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        """ sets in __objects the obj with key <obj class name>.id"""
+        key_obj = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key_obj] = obj
+
+    def save(self):
+        """
+        this method opens file via file_path and serialize the python
+        objet in to a json one using the json.dump function
+        """  
+        with open(self.__file_path, "w", encoding="utf-8") as f:
+            json.dump({k: v.to_dict() for k, v in self.__objects.items()}, f)
+            #json.dump(self.__objects, f)
+
+
+    def reload(self):
+        """
+        This module decerialize the json string from file
+        in to a python object
+         """
+        try:
+            with open(self.__file_path, "r", encoding="utf-8") as f:
+                self.__objects = {k: BaseModel(**v) for k, v in json.load(f).items()} 
+        except Exception:
+            pass
+
+    
+    
